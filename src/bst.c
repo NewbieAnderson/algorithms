1e909853bcb15831a89f8c1847f0eda03fe09ebb
NewbieAnderson <engineeringsong@gmail.com>
2023-04-22 21:45:37 +0900

    implemented bst functions

--- src/bst.c
+++ src/bst.c
@@ -1,7 +1,28 @@
 #include "bst.h"
 
-void bst_retrieve(const struct bst_node *const root, const unsigned char traverse_type)
+void traverse_pre_order_recursive(const struct bst_node *const node)
 {
+    if (node == NULL)
+        return;
+    printf("%d\n", node->data);
+    traverse_pre_order_recursive(node->left);
+    traverse_pre_order_recursive(node->right);
+}
+
+void traverse_in_order_recursive(const struct bst_node *const node)
+{
+    if (node == NULL)
+        return;
+    traverse_in_order_recursive(node->left);
+    printf("%d\n", node->data);
+    traverse_in_order_recursive(node->right);
+}
+
+void traverse_post_order_recursive(const struct bst_node *const node)
+{
+    traverse_post_order_recursive(node->left);
+    printf("%d\n", node->data);
+    traverse_post_order_recursive(node->right);
 }
 
 const struct bst_node *const bst_search(const struct bst_node *const root, const int data)
@@ -51,14 +72,54 @@ int bst_insert(const struct bst_node *const root, const int data)
 
 void bst_delete(const struct bst_node *const root, const int data)
 {
-}
-
-int arr_to_bst(const struct bst_node *const root, int arr[], int len)
-{
-    return 0;
-}
-
-int bst_to_arr(const struct bst_node *const root, int arr[], int len)
-{
-    return 0;
+    struct bst_node *node = root;
+    struct bst_node *parent_node = NULL;
+    struct bst_node *subtree_node = NULL;
+    struct bst_node *parent_subtree_node = NULL;
+    while (node != NULL && node->data != data) {
+        parent_node = node;
+        node = node->data > data ? node->left : node->right;
+    }
+    if (node == NULL)
+        return;
+    if (node->left != NULL && node->right != NULL) {
+        subtree_node = node->left;
+        while (subtree_node->right) {
+            parent_subtree_node = subtree_node;
+            subtree_node = subtree_node->right;
+        }
+        node->data = subtree_node->data;
+        free(subtree_node);
+        subtree_node = NULL;
+    } else if (node->left != NULL) {
+        if (parent_node->left == node)
+            parent_node->left = node->left;
+        else if (parent_node->right == node)
+            parent_node->right = node->left;
+        free(node);
+        node = NULL;
+    } else if (node->right != NULL) {
+        if (parent_node->left == node)
+            parent_node->left = node->right;
+        else if (parent_node->right == node)
+            parent_node->right = node->right;
+        free(node);
+        node = NULL;
+    } else {
+        if (parent_node != NULL) {
+            if (parent_node->left == node)
+                parent_node->left = NULL;
+            else if (parent_node->right == node)
+                parent_node->right = NULL;
+        }
+        free(node);
+        node = NULL;
+        return;
+    }
+    if (parent_subtree_node != NULL) {
+        if (parent_subtree_node->left == subtree_node)
+            parent_subtree_node->left = NULL;
+        else if (parent_subtree_node->right == subtree_node)
+            parent_subtree_node->right = NULL;
+    }
 }
--- src/bst.h
+++ src/bst.h
@@ -3,10 +3,6 @@
 
 #include <stdlib.h>
 
-#define PRE_ORDER_TRAVERSAL  1
-#define IN_ORDER_TRAVERSAL   2
-#define POST_ORDER_TRAVERSAL 3
-
 struct bst_node {
     struct bst_node *left;
     struct bst_node *right;
@@ -14,16 +10,16 @@ struct bst_node {
     unsigned char is_dynamic;
 };
 
-void bst_retrieve(const struct bst_node *const root, const unsigned char traverse_type);
+void traverse_pre_order_recursive(const struct bst_node *const node);
+
+void traverse_in_order_recursive(const struct bst_node *const node);
+
+void traverse_post_order_recursive(const struct bst_node *const node);
 
 const struct bst_node *const bst_search(const struct bst_node *const root, const int data);
 
 int bst_insert(const struct bst_node *const root, const int data);
 
 void bst_delete(const struct bst_node *const root, const int data);
-
-int arr_to_bst(const struct bst_node *const root, int arr[], int len);
-
-int bst_to_arr(const struct bst_node *const root, int arr[], int len);
 
 #endif
