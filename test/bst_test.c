3cf1a96bf9240ce9d02491ed6093810a5976caad
NewbieAnderson <engineeringsong@gmail.com>
2023-04-26 23:56:48 +0900

    bst test code

--- test/bst_test.c
+++ test/bst_test.c
@@ -9,11 +9,23 @@ int main(void)
     bst_insert(root, 1);
     bst_search(root, 3);
     traverse_in_order_recursive(root);
+    printf("try to delete node 32\n");
     bst_delete(root, 32);
+    printf("try to delete node 5\n");
     bst_delete(root, 5);
+    printf("try to delete node 5\n");
+    bst_delete(root, 5);
+    printf("try to delete node 5\n");
+    bst_delete(root, 5);
+    printf("try to delete node 2\n");
     bst_delete(root, 2);
+    printf("try to delete node 3\n");
     bst_delete(root, 3);
+    printf("try to delete node 12\n");
+    bst_delete(root, 12);
+    printf("try to delete node 1\n");
     bst_delete(root, 1);
+    printf("try to delete node 3\n");
     bst_delete(root, 3);
     return 0;
 }
