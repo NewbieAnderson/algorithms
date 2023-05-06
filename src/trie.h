275ee79256005c6b4f0542e892c78403b9fd3c12
NewbieAnderson <engineeringsong@gmail.com>
2023-05-06 15:42:10 +0900

    created trie header file

---
+++ src/trie.h
@@ -0,0 +1,21 @@
+#ifndef TRIE_H
+#define TRIE_H
+
+#define ALPHABET_COUNTS_UPPER_LOWER 52
+
+struct trie_node {
+    struct trie_node *nodes[ALPHABET_COUNTS_UPPER_LOWER];
+    char ch;
+};
+
+void trie_init(struct trie_node *root);
+
+void trie_insert(const char *str);
+
+void trie_delete_str(const char *str);
+
+void trie_auto_compelete(const char *str);
+
+void trie_free(struct trie_node *root);
+
+#endif
