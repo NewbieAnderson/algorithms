8198c75b2fbb653baf9f2eee82c5f2cbfc6d897d
NewbieAnderson <engineeringsong@gmail.com>
2023-05-08 23:21:38 +0900

    change trie features

---
+++ src/trie.c
@@ -0,0 +1,21 @@
+#include "trie.h"
+
+void trie_init(struct trie_node *root)
+{
+}
+
+void trie_insert_str(const char *str)
+{
+}
+
+void trie_delete_str(const char *str)
+{
+}
+
+void trie_auto_compelete(const char *str)
+{
+}
+
+void trie_release_all(struct trie_node *root)
+{
+}
--- src/trie.h
+++ src/trie.h
@@ -10,12 +10,12 @@ struct trie_node {
 
 void trie_init(struct trie_node *root);
 
-void trie_insert(const char *str);
+void trie_insert_str(const char *str);
 
 void trie_delete_str(const char *str);
 
 void trie_auto_compelete(const char *str);
 
-void trie_free(struct trie_node *root);
+void trie_release_all(struct trie_node *root);
 
 #endif
